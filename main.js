7e51d5484df0bb111d3dd2a167a390217eabad2f
muhammaddiyor5 <92600089+muhammaddiyor5@users.noreply.github.com>
2021-11-08 18:01:28 +0500

    finish

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("salom");
+// setTimeout(function () {
+//   alert("salom");
+// }, 1000);
+window.addEventListener("DOMContentLoaded", function () {
+  const loader = document.querySelector(".loader");
+
+  setTimeout(function () {
+    loader.style.opacity = "0";
+    setTimeout(function () {
+      loader.style.display = "none";
+    }, 500);
+  }, 2000);
+});
